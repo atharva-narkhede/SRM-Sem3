048094c6fff4d51784d8df50b1efc7b0811b4600
Atharva Narkhede <106006803+atharva-narkhede@users.noreply.github.com>
2022-11-01 11:27:29 +0530

    Create Waiter.py

---
+++ ISCP/01-11-22 Practice Test/Waiter.py
@@ -0,0 +1,39 @@
+#You are a waiter at a party. There is a pile of numbered plates. Create an empty  array. At each iteration, , remove each plate from the top of the stack in order. Determine if the number on the plate is evenly divisible by the  prime number. If it is, stack it in pile . Otherwise, stack it in stack . Store the values in  from top to bottom in . In the next iteration, do the same with the values in stack . Once the required number of iterations is complete, store the remaining values in  in , again from top to bottom. Return the  array. hackerrank
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'waiter' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY number
+#  2. INTEGER q
+#
+
+def waiter(number, q):
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    q = int(first_multiple_input[1])
+
+    number = list(map(int, input().rstrip().split()))
+
+    result = waiter(number, q)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
